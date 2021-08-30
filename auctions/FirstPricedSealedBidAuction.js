b896a155703aef78ff6d9566fd841a89000e25dd
khyatith <thakur.khyati70@gmail.com>
2021-08-29 23:12:52 -0700

    first priced sealed bid auction code

---
+++ auctions/FirstPricedSealedBidAuction.js
@@ -0,0 +1,40 @@
+let { FIRST_PRICED_SEALED_BID_AUCTIONS } = require('../constants');
+
+class FirstPricedSealedBidAuction {
+
+  constructor(currentAuctionObject, bidTeam, bidAmount, timestamp) {
+    this.currentAuctionObject = currentAuctionObject;
+    this.bidTeam = bidTeam;
+    this.bidAmount = bidAmount;
+    this.timestamp = timestamp;
+  }
+
+  updateBidObject() {
+    console.log('inside updated bid object');
+    FIRST_PRICED_SEALED_BID_AUCTIONS.push({
+      auctionObj: this.currentAuctionObject,
+      bidTeam: this.bidTeam,
+      bidAmount: this.bidAmount,
+      timestamp: this.timestamp
+    });
+    return FIRST_PRICED_SEALED_BID_AUCTIONS;
+  }
+
+  calculateWinner() {
+    const winner = FIRST_PRICED_SEALED_BID_AUCTIONS.reduce((acc, obj) => {
+      if (acc.bidAmount === obj.bidAmount) {
+        if (acc.bidAt < obj.bidAt) {
+          return acc;
+        } else {
+          return obj;
+        }
+      }
+      return (acc.bidAmount > obj.bidAmount) ? acc : obj;
+    }, {});
+    console.log('winner', winner);
+    return winner;
+    //const maxBidAmount = Math.max(...allBidAmounts);
+  }
+}
+
+module.exports.FirstPricedSealedBidAuction = FirstPricedSealedBidAuction;
