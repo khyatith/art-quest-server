9536ec01429548b7baab133e72e8cfd078d68096
khyatith <thakur.khyati70@gmail.com>
2021-08-31 08:46:10 -0700

    Update server

--- server.js
+++ server.js
@@ -7,25 +7,25 @@ const io = require("socket.io")(5000, {
 });
 require("dotenv").config();
 
-const currentTime = Date.parse(new Date());
 let currentAuction = {};
 
 // 10 mins landing page timer
-const landingPageTimerDeadline = new Date(currentTime + 1*60*1000);
+let landingPageTimerDeadline;
 let landingPageTimeInterval;
-let landingPageTimerEnded = false;
+let landingPageTimerStarted = false;
 
-console.log('currentTime', currentTime);
+
 let auctionsTimer;
 let isAuctionTimerStarted = false;
 let auctionTimerInterval;
 
 function updateLandingPageClock() {
+  landingPageTimerStarted = true;
   const t = getRemainingTime(landingPageTimerDeadline);
   if (t.total <= 0) {
-    landingPageTimerEnded = true;
+    landingPageTimerStarted = false;
     clearInterval(landingPageTimeInterval);
-  } else if (!landingPageTimerEnded && t.total > 0) {
+  } else if (landingPageTimerStarted && t.total > 0) {
     io.emit("landingPageTimerValue", t);
   }
 }
@@ -34,11 +34,9 @@ async function updateAuctionClock() {
   isAuctionTimerStarted = true;
   const t = getRemainingTime(auctionsTimer);
   if (t.total <=0) {
-    console.log('inside t.total <=0', t);
     isAuctionTimerStarted = false;
     clearInterval(auctionTimerInterval);
     const bidWinner = getBidWinner(currentAuction);
-    console.log('bidWinner', bidWinner);
     if (bidWinner) {
       currentAuction = updateAuctionState(currentAuction, 2);
       io.emit("displayBidWinner", bidWinner);
@@ -77,16 +75,19 @@ io.on("connection", socket => {
   });
   
   socket.on("startLiveAuctions", () => {
-    console.log('ongoingAuction', currentAuction);
+    console.log('inside start live auctions');
     if (!currentAuction || currentAuction.auctionState !== 1) {
-      console.log('ongoingAuction inside if', currentAuction);
+      console.log('inside current auction');
       currentAuction = getNextObjectForLiveAuction();
     }
-    console.log('ongoingAuction after if', currentAuction);
     socket.emit("startNextAuction", currentAuction);
   });
 
   socket.on("startLandingPageTimer", timerInMinutes => {
+    if (!landingPageTimerStarted) {
+      const currentTime = Date.parse(new Date());
+      landingPageTimerDeadline = new Date(currentTime + 1*60*1000);
+    }
     landingPageTimeInterval = setInterval(updateLandingPageClock, 1000);
   });
 
@@ -99,7 +100,6 @@ io.on("connection", socket => {
   });
 
   socket.on("addNewBid", bidInfo => {
-    console.log('inside allbids winner', bidInfo);
     const { auctionType } = bidInfo;
     switch(auctionType) {
       case '1':
