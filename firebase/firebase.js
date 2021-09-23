249e64b23cd408b3e4656222af2225f0c21d8bed
khyatith <thakur.khyati70@gmail.com>
2021-09-22 20:44:08 -0700

    Add firebase creds for heroku

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,11 +1,17 @@
-var admin = require("firebase-admin");
+var firebaseAdminSdk = require("firebase-admin");
+let firebaseAdminApp;
 
-var serviceAccount = require("../auctions-service-key.json");
+if (process.env.FIREBASE_SERVICE_KEYS) {
+	firebaseAdminApp = firebaseAdminSdk.initializeApp({credential: firebaseAdminSdk.credential.cert(
+		JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_KEYS, 'base64').toString('ascii')))
+	});
+} else {
+	const serviceAccount = require("../auctions-service-key.json");
+	firebaseAdminApp = firebaseAdminSdk.initializeApp({
+		credential: firebaseAdminSdk.credential.cert(serviceAccount),
+	});
+}
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
+const db = firebaseAdminApp.firestore();
 
 module.exports = { db };
