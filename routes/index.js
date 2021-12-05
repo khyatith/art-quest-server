913be6235617d8d74907f11f01faf2ba78660d1a
khyatith <thakur.khyati70@gmail.com>
2021-12-04 19:37:32 -0800

    Increase timers

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.get('/timer/:hostCode', function (req, res) {
     res.send({ landingPageTimerValue: room.landingPageTimerValue });
   } else {
     const currentTime = Date.parse(new Date());
-    const deadline = new Date(currentTime + 0.3 * 60 * 1000);
+    const deadline = new Date(currentTime + 3 * 60 * 1000);
     const timerValue = getRemainingTime(deadline);
     setInterval(() => startServerTimer(room, deadline), 1000);
     res.send({ landingPageTimerValue: timerValue });
@@ -120,7 +120,7 @@ router.get('/auctionTimer/:hostCode/:auctionId', function (req, res) {
     res.send({ currentAuctionObjTimer: currentAuctionObj.auctionTimerValue });
   } else {
     const currentTime = Date.parse(new Date());
-    const deadline = new Date(currentTime + 0.5 * 60 * 1000);
+    const deadline = new Date(currentTime + 1 * 60 * 1000);
     const timerValue = getRemainingTime(deadline);
     setInterval(() => startAuctionServerTimer(room, currentAuctionObj, deadline), 1000);
     res.send({ currentAuctionObjTimer: timerValue });
