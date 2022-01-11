92c4e0adfebc4c12a508e95bc55e47ccf71466f0
gsc Mac <gscmac@gscs-MacBook-Air.local>
2022-01-10 20:44:08 -0800

    update timer

--- routes/index.js
+++ routes/index.js
@@ -396,7 +396,7 @@ mongoClient.then(db => {
                 selling_info.sellPaintingTimerValue = room.sellPaintingTimerValue;
               } else {
                 const currentTime = Date.parse(new Date());
-                const deadline = new Date(currentTime + 0.3 * 60 * 1000);
+                const deadline = new Date(currentTime + 0.5 * 60 * 1000);
                 const timerValue = getRemainingTime(deadline);
                 setInterval(() => startSellingServerTimer(room, deadline), 1000);
                 selling_info.sellPaintingTimerValue = timerValue;
